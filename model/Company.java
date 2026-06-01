9a7c26b89ae13fc41a235fe655fe2c132b8864f7
Ramyaa Murugan <87483351+RAMYAA-12@users.noreply.github.com>
2026-06-01 21:17:58 +0530

    Create Company.java

---
+++ model/Company.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Company {
+    private int id;
+    private String companyName;
+
+    public Company(int id, String companyName) {
+        this.id = id;
+        this.companyName = companyName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+}
